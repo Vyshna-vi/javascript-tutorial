e2a0a66e97ee755ffe17d55deb1617c9d8491d0a
vyshna-vi <vyshnaviponnuzz00012gmail.com>
2022-11-01 08:17:36 +0530

    completed numberoperations

---
+++ 02-numberop.js
@@ -0,0 +1,45 @@
+// console.log("numberoperations")
+console.log(10*10);
+console.log(100/20);
+console.log(80+20);
+console.log(120-20);
+let a=2;
+let b=10;
+let x=4;
+let y=20;
+console.log(a*b);
+console.log(a/b);
+console.log(a-b);
+console.log(a+b);
+console.log((a+b)-(x*y));
+console.log(y%a);
+a++;
+console.log(a++);
+console.log(++a);
+let num=10;
+// let n=num++;
+num++;
+console.log(num);
+// n=++num;
+++num;
+console.log(num);
+// augmented addition
+let v=10;
+v=v+5;
+console.log(v);
+// augmented substraction
+let j=10;
+j=j-5;
+console.log(j);
+// augmented + -  / *
+let s=4;
+s/=2;
+console.log(s);
+s*=2;
+console.log(s);
+s+=2;
+console.log(s);
+s-=2;
+console.log(s);
+let h=120;
+console.log(typeof(h));
