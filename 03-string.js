c845a5c1158d89535abc5a288bd67ab9f456e27c
vyshna-vi <vyshnaviponnuzz00012gmail.com>
2022-11-01 09:06:56 +0530

    completed stringoperations

---
+++ 03-string.js
@@ -0,0 +1,20 @@
+// concatenate is to combine two strigs.
+let x="Hello ";
+let y="World";
+let a=x+y;
+console.log(a);
+// to take a letter from String(position startig from 0)
+console.log(x[3]);
+console.log(y[4]);
+// To find number of characters
+let j="NEVER GIVE UP ON YOUR DREAMS";
+console.log(j.length);
+console.log(j[j.length-1]);//to find last character
+console.log(j[6]);
+// string split into Array
+let k="hello";
+let arr=k.split("");
+console.log(arr);
+let v="keep smiling";
+let d=v.split("i");
+console.log(d);
